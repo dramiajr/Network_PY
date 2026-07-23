31717862bafad168dd3a913f697c77a6a41215e3
David Ramia Jr <davidramiajr@gmail.com>
2026-07-23 14:56:04 -0500

    Add files via upload

---
+++ Initial_Troubleshooting_Report/backend/api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from initial_sw_troubleshooting import validate_ip, ping_target_ip
+
+
+app = FastAPI()
+
+@app.get("/health")
+def health_check():
+    status = {
+        "update": "working"
+    }
+    return status 
+
+@app.get("/ping")
+def run_ping(ip_address: str):
+    valid_ip = validate_ip(ip_address)
+    if valid_ip == False:
+        return valid_ip
+    else:
+        ping_attempt = ping_target_ip(ip_address)
+        return ping_attempt
+
+
+
+    
