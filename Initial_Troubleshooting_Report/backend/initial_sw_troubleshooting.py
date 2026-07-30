3ece4dfb2948e8478ff7113738157895b8449d12
David Ramia Jr. <davidramiajr@gmail.com>
2026-07-30 14:46:39 -0500

    Added target interface validation, prompt for user credentials, & netmiko ssh + show interfaces<target interface> command

--- Initial_Troubleshooting_Report/backend/initial_sw_troubleshooting.py
+++ Initial_Troubleshooting_Report/backend/initial_sw_troubleshooting.py
@@ -1,4 +1,6 @@
 import subprocess
+import getpass
+from netmiko import ConnectHandler 
 
 # IP Validation Logic 
 def validate_ip(ip_address):
@@ -38,6 +40,7 @@ def ping_target_ip(ip_address):
     
     # Use for Windows
     #ping_command = ["ping", "-4", "-n", "4", ip_address]
+
     ping_attempt = subprocess.run(
         ping_command,
         capture_output=True,
@@ -52,23 +55,103 @@ def ping_target_ip(ip_address):
     else:
         ping_status ="successful"
         
-
     stored_ping_result = {
         "ping_status": ping_status,
         "ping_output_raw": ping_output 
     }
     return stored_ping_result
 
+# Interface Type Validation Logic
+def validate_interface_type(interface_type):
+    approved_int_type = ["Fa", "Gi", "Te"]
+    if interface_type not in approved_int_type:
+        return False
+    else:
+        return True
+
+# Interface Nummber Validation Logic
+def validate_interface_number(interface_number):
+    split_int = interface_number.split("/")
+
+    if len(split_int) == 2 or len(split_int) == 3:
+        for section in split_int:
+            if section == "":
+                return False
+            elif len(section) > 3:
+                return False
+            elif not section.isdigit():
+                return False
+        return True
+    else:
+        return False
+
+# Get Interface from User
+def get_interface():
+    interface_type = input("Enter Interface Type (Fa, Gi, Te): ")
+    valid_int_type = validate_interface_type(interface_type)
+    while valid_int_type == False:
+        interface_type = input("Enter Interface Type (Fa, Gi, Te): ")
+        valid_int_type = validate_interface_type(interface_type)
+    
+    interface_number = input("Enter Interface: ")
+    valid_int_num = validate_interface_number(interface_number)
+    while valid_int_num == False:
+        interface_number = input("Enter Interface: ")
+        valid_int_num = validate_interface_number(interface_number)
+
+    valid_interface = interface_type + interface_number
+
+    return valid_interface
+
+
+def get_ssh_credentials():
+    username = input("Enter Username: ")
+    while username == "":
+        username = input("Enter Username: ")
+
+    password = getpass.getpass("Enter Password: ", echo_char="*")
+    while password == "":
+        password = getpass.getpass("Enter Password: ", echo_char="*")
+
+    ssh_credentials = {
+        "username" : username,
+        "password" : password
+    }
+
+    return(ssh_credentials)  
+
+def netmiko_device_information(ip_address, username, password):
+    device_info = {
+        "device_type" : "cisco_ios",
+        "host" : ip_address,
+        "username" : username,
+        "password": password
+    }
+
+    return device_info
+
 
 
 def main():
     target_ip = get_target_ip()
-
+    target_interface = get_interface()
+    user_credentials = get_ssh_credentials()
+    stored_device_information = netmiko_device_information(target_ip, user_credentials["username"], user_credentials["password"])
     stored_ping_result = ping_target_ip(target_ip)
 
+    net_connect = ConnectHandler(**stored_device_information)
+    show_interfaces_target = net_connect.send_command(f"show interfaces {target_interface}")
+
+    print(f"\n{show_interfaces_target}")
+    net_connect.disconnect()
+#"""
     print("\nICMP Checks")
     print("Status:", stored_ping_result["ping_status"])
     print(stored_ping_result["ping_output_raw"])
+
+
+#"""
+
 
 if __name__ == "__main__":
     main()
